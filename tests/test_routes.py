a5e4df60dfbb72a3cc4d5ddbd07491d0f8ebeb03
arekmurzyn <arek.murzyn@gmail.com>
2024-05-07 06:29:24 -0400

    updates

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -29,7 +29,6 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
-
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
